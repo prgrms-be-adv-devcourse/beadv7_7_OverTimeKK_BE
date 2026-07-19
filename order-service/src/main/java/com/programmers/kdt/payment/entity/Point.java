c318b66ebed0e4f8e6728d7f78f75bb870f1b633
Yuhyun <rladbgus781@gmail.com>
2026-07-19 17:13:16 +0900

    feat: 포인트 엔티티 및 비즈니스 로직 구현

--- order-service/src/main/java/com/programmers/kdt/payment/entity/Point.java
+++ order-service/src/main/java/com/programmers/kdt/payment/entity/Point.java
@@ -15,6 +15,45 @@ public class Point extends BaseTimeEntity {
     @Id
     private Long userId;
 
-    @Column(name = "total_amount", nullable = false)
-    private Long point;
+    @Column(name = "total_point", nullable = false)
+    private Long totalPoint;
+
+    public static Point create(Long userId) {
+        if (userId == null) {
+            throw new IllegalArgumentException("userId가 없습니다.");
+        }
+        Point point = new Point();
+        point.userId = userId;
+        point.totalPoint = 0L; // 초기 포인트는 0원
+        return point;
+    }
+
+    // 포인트 적립
+    public void earn(Long amount) {
+        add(amount, "적립");
+    }
+
+    // 포인트 사용
+    public void use(Long amount) {
+        if (amount == null || amount <= 0) {
+            throw new IllegalArgumentException("사용 금액은 0원보다 커야 합니다. 사용금 : " + amount);
+        }
+        if (this.totalPoint < amount) {
+            throw new IllegalArgumentException("사용 금액이 보유 포인트보다 많습니다. 보유 포인트 : " + this.totalPoint + ", 사용금 : " + amount);
+        }
+        this.totalPoint -= amount;
+    }
+
+    // 포인트 환급
+    public void rollbackUse(Long amount) {
+        add(amount, "환급");
+    }
+
+    // 지급, 환급시 공통 메서드
+    private void add(Long amount, String label) {
+        if (amount == null || amount <= 0) {
+            throw new IllegalArgumentException(label + "금액은 0원보다 커야 합니다. 금액 : " + amount);
+        }
+        this.totalPoint += amount;
+    }
 }
