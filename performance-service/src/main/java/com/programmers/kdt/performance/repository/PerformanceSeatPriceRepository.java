24844aa0811c35d2f8848b2079a4906bb8972a88
yosep98 <dytpq0978@naver.com>
2026-07-19 15:42:30 +0900

    refactor: 리포지토리 조회 Performance 객체 파라미터로 통일

--- performance-service/src/main/java/com/programmers/kdt/performance/repository/PerformanceSeatPriceRepository.java
+++ performance-service/src/main/java/com/programmers/kdt/performance/repository/PerformanceSeatPriceRepository.java
@@ -1,10 +1,11 @@
 package com.programmers.kdt.performance.repository;
 
+import com.programmers.kdt.performance.entity.Performance;
 import com.programmers.kdt.performance.entity.PerformanceSeatPrice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface PerformanceSeatPriceRepository extends JpaRepository<PerformanceSeatPrice, Long> {
-    Optional<PerformanceSeatPrice> findByPerformance_PerformanceInfoIdAndHallIdAndZone(
-            Long performanceInfoId, Long hallId, String zone);
+    Optional<PerformanceSeatPrice> findByPerformanceAndHallIdAndZone(
+            Performance performance, Long hallId, String zone);
 }
--- performance-service/src/main/java/com/programmers/kdt/performance/repository/PerformanceSessionRepository.java
+++ performance-service/src/main/java/com/programmers/kdt/performance/repository/PerformanceSessionRepository.java
@@ -1,10 +1,11 @@
 package com.programmers.kdt.performance.repository;
 
+import com.programmers.kdt.performance.entity.Performance;
 import com.programmers.kdt.performance.entity.PerformanceSession;
 import com.programmers.kdt.performance.entity.PerformanceSessionId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PerformanceSessionRepository extends JpaRepository<PerformanceSession, PerformanceSessionId> {
-    List<PerformanceSession> findByPerformanceSessionId_PerformanceInfoId(Long performanceInfoId);
+    List<PerformanceSession> findByPerformance(Performance performance);
 }
