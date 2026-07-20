6138693b0d97a14d7109ddb9cbe95dd5b8534d70
ynzzi <syg7474@naver.com>
2026-07-20 14:30:24 +0900

    test: 주문 엔티티 생성 테스트 추가

--- order-service/src/main/java/com/programmers/kdt/order/service/OrderService.java
+++ order-service/src/main/java/com/programmers/kdt/order/service/OrderService.java
@@ -2,9 +2,6 @@ package com.programmers.kdt.order.service;
 
 import com.programmers.kdt.order.dto.CreateOrderRequest;
 import com.programmers.kdt.order.dto.CreateOrderResponse;
-import com.programmers.kdt.order.repository.OrderRepository;
-import com.programmers.kdt.payment.service.PaymentService;
-import com.programmers.kdt.ticket.service.TicketService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,10 +10,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OrderService {
 
-    private final OrderRepository orderRepository;
-    private final TicketService ticketService;
-    private final PaymentService paymentService;
-    private final UserClient userClient;
 
     @Transactional
     public CreateOrderResponse createOrder(CreateOrderRequest request) {
