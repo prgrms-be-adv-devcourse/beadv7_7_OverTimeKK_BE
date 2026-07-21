64b6aab9fb6fc6b248ad66baa7fd561d51f7d0bb
happ-in <gpqls304@naver.com>
2026-07-21 20:06:41 +0900

    feat: 회차정보 삭제 API 구현

--- performance-service/src/main/java/com/programmers/kdt/performance/exception/PerformanceErrorCode.java
+++ performance-service/src/main/java/com/programmers/kdt/performance/exception/PerformanceErrorCode.java
@@ -7,11 +7,12 @@ public enum PerformanceErrorCode implements ErrorCode {
     INVALID_PERIOD(HttpStatus.BAD_REQUEST, "PFM400_001", "공연 종료일은 시작일 이후여야 합니다."),
     INVALID_TICKET_OPEN(HttpStatus.BAD_REQUEST, "PFM400_002", "티켓 오픈 시각은 공연 시작 전이어야 합니다."),
     PERFORMANCE_NOT_FOUND(HttpStatus.NOT_FOUND, "PFM404_001", "공연을 찾을 수 없습니다."),
+    PERFORMANCE_SESSION_NOT_FOUND(HttpStatus.NOT_FOUND, "P404_002", "공연 회차 정보를 찾을 수 없습니다."),
     NOT_PERFORMANCE_OWNER(HttpStatus.FORBIDDEN, "PFM403_001", "해당 공연의 판매자가 아닙니다."),
-    PERFORMANCE_SESSION_NOT_FOUND(HttpStatus.UNPROCESSABLE_CONTENT, "P422_002", "공연 회차 정보를 찾을 수 없습니다."),
-    PERFORMANCE_SESSION_NOT_VALID(HttpStatus.UNPROCESSABLE_CONTENT, "P422_010", "공연 정보가 유효하지 않습니다."),
-    PERFORMANCE_SESSION_INVALID_START_TIME(HttpStatus.UNPROCESSABLE_CONTENT, "P422_011", "공연 시작 시간은 현재보다 이후여야 합니다."),
-    PERFORMANCE_SESSION_UPDATE_NOT_ALLOWED_AFTER_TICKET_OPEN(HttpStatus.UNPROCESSABLE_CONTENT, "P422_12", "공연 티켓 발권 시작 후에는 공연 정보 변경이 불가능합니다.");
+    PERFORMANCE_SESSION_NOT_VALID(HttpStatus.UNPROCESSABLE_CONTENT, "P422_001", "공연 정보가 유효하지 않습니다."),
+    PERFORMANCE_SESSION_INVALID_START_TIME(HttpStatus.UNPROCESSABLE_CONTENT, "P422_002", "공연 시작 시간은 현재보다 이후여야 합니다."),
+    PERFORMANCE_SESSION_UPDATE_NOT_ALLOWED_AFTER_TICKET_OPEN(HttpStatus.UNPROCESSABLE_CONTENT, "P422_003", "공연 티켓 발권 시작 후에는 회차 정보 {0} 불가 합니다."),
+    PERFORMANCE_SESSION_UPDATE_NOT_ALLOWED_AFTER_PERFORMANCE_START(HttpStatus.UNPROCESSABLE_CONTENT, "P422_004", "공연 시작 후에는 회차 정보 {0} 불가 합니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
