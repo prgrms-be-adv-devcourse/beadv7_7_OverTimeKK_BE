37447b4c46e2c8e40c464c55674ec251bd8af00f
happ-in <gpqls304@naver.com>
2026-07-21 17:32:40 +0900

    refactor: 와일드카드 import를 명시적 import로 변경

--- performance-service/src/main/java/com/programmers/kdt/venue/entity/Hall.java
+++ performance-service/src/main/java/com/programmers/kdt/venue/entity/Hall.java
@@ -1,7 +1,16 @@
 package com.programmers.kdt.venue.entity;
 
 import com.programmers.kdt.common.entity.BaseTimeEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -19,7 +28,7 @@ public class Hall extends BaseTimeEntity {
     @Column(name = "hall_id")
     private Long hallId;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "venue_id", nullable = false)
     private Venue venue;
 
