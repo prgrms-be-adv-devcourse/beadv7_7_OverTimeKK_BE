c4fa446a68ecdcc9af7a0ae32b7ba73cad9a5001
ynzzi <syg7474@naver.com>
2026-07-21 12:15:23 +0900

    feat: 주문 엔티티 테스트코드 작성 및 주문에러코드 위치 변경

--- order-service/src/main/java/com/programmers/kdt/order/exception/OrderErrorCode.java
+++ order-service/src/main/java/com/programmers/kdt/order/exception/OrderErrorCode.java
@@ -1,14 +1,17 @@
-package com.programmers.kdt.common.exception;
+package com.programmers.kdt.order.exception;
 
+import com.programmers.kdt.common.exception.ErrorCode;
 import org.springframework.http.HttpStatus;
 
 public enum OrderErrorCode implements ErrorCode {
 
-    ORDER_NOT_PENDING(HttpStatus.CONFLICT, "ORD409_1", "결제 대기 상태 주문만 완료할 수 있습니다."),
-    ORDER_NOT_COMPLETED(HttpStatus.CONFLICT, "ORD409_2", "완료된 주문만 취소할 수 있습니다."),
     TICKET_ID_REQUIRED(HttpStatus.BAD_REQUEST, "ORD400_1", "주문할 티켓 정보가 필요합니다."),
     USER_ID_REQUIRED(HttpStatus.BAD_REQUEST, "ORD400_2", "주문자 ID는 필수입니다."),
-    INVALID_ORDER_AMOUNT(HttpStatus.BAD_REQUEST, "ORDER400_3", "주문 금액은 0보다 커야 합니다.");
+    INVALID_ORDER_AMOUNT(HttpStatus.BAD_REQUEST, "ORD400_3", "주문 금액은 0보다 커야 합니다."),
+    ORDER_ITEMS_REQUIRED(HttpStatus.BAD_REQUEST, "ORD400_4", "주문할 상품이 없습니다."),
+    ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "ORD404_1", "주문을 찾을 수 없습니다."),
+    ORDER_NOT_PENDING(HttpStatus.CONFLICT, "ORD409_1", "결제 대기 상태 주문만 완료할 수 있습니다."),
+    ORDER_NOT_COMPLETED(HttpStatus.CONFLICT, "ORD409_2", "완료된 주문만 취소할 수 있습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
