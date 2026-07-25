666d983b0ea4f11f10494bbf3ac18de4ed56c410
happ-in <gpqls304@naver.com>
2026-07-25 23:10:48 +0900

    refactor : request valid notnull 반환 메시지값 설정

--- performance-service/src/main/java/com/programmers/kdt/ticket/dto/CheckTicketHoldAvailableRequest.java
+++ performance-service/src/main/java/com/programmers/kdt/ticket/dto/CheckTicketHoldAvailableRequest.java
@@ -3,7 +3,10 @@ package com.programmers.kdt.ticket.dto;
 import jakarta.validation.constraints.NotNull;
 
 public record CheckTicketHoldAvailableRequest(
-        @NotNull Long ticketId,
-        @NotNull Long userId
+        @NotNull(message  = "티켓 정보를 입력해주세요.")
+        Long ticketId,
+
+        @NotNull(message = "주문자 정보를 입력해주세요.")
+        Long userId
 ) {
 }
