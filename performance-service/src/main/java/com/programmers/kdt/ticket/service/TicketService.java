7f66b417b34df884a45d14619a895823b6b6f268
happ-in <gpqls304@naver.com>
2026-07-27 12:29:19 +0900

    refactor: RestAPI → Event으로 변경

--- performance-service/src/main/java/com/programmers/kdt/ticket/service/TicketService.java
+++ performance-service/src/main/java/com/programmers/kdt/ticket/service/TicketService.java
@@ -1,19 +1,19 @@
 package com.programmers.kdt.ticket.service;
-import com.programmers.kdt.ticket.dto.CreateStandbyResponse;
+
+import com.programmers.kdt.standby.event.StandbyTicketEvent;
 import com.programmers.kdt.ticket.dto.CheckTicketHoldAvailableRequest;
 import com.programmers.kdt.ticket.dto.CheckTicketHoldAvailableResponse;
+import com.programmers.kdt.ticket.dto.CreateStandbyResponse;
 import com.programmers.kdt.ticket.dto.SessionStartDateResponse;
-import com.programmers.kdt.ticket.dto.StandbyTicketRequest;
 
 public interface TicketService {
     CreateStandbyResponse issueStandby(Long userId, Long sessionNum, String zone);
 
     SessionStartDateResponse getSessionStartDate(Long ticketId);
 
-    /* 좌석점유 상태 확인, 점유가능한 상태면 hold하고 만료시간 return */
     CheckTicketHoldAvailableResponse checkTicketHoldStatus(CheckTicketHoldAvailableRequest request);
 
     void releaseHoldTicket(Long ticketId);
 
-    void standbyTicket(StandbyTicketRequest request);
+    void standbyTicket(StandbyTicketEvent event);
 }
