62a9b956a62a6eec016dc7abe2b6597477e88936
happ-in <gpqls304@naver.com>
2026-08-24 09:51:06 +0900

    refactor: userId 값 어디선가 사라진거 다시 생성

--- performance-service/src/main/java/com/programmers/kdt/ticket/dto/CheckTicketHoldAvailableRequest.java
+++ performance-service/src/main/java/com/programmers/kdt/ticket/dto/CheckTicketHoldAvailableRequest.java
@@ -7,6 +7,9 @@ public record CheckTicketHoldAvailableRequest(
         @NotNull(message  = "티켓 정보를 입력해주세요.")
         Long ticketId,
 
+        @NotNull(message = "주문자 정보를 입력해주세요.")
+        Long userId,
+
         @NotBlank(message = "일반/대기주문 케이스를 입력해주세요.")
         String orderType
 ) {
